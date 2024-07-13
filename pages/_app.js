00d619daada2c5f29ec8f21835780ed9d017cf7e
pranav-kural <pranavkural042@gmail.com>
2024-07-13 13:30:18 -0400

    added vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/global.css";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   // injext vercel analytics
   inject();
+  injectSpeedInsights();
   return <Component {...pageProps} />;
 }
